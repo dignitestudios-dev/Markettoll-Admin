bec8302e55f3c81493adc4cca3d3a55522104e50
zackcole <183650880+zackcole@users.noreply.github.com>
2025-08-11 00:01:12 -0700

    update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,30 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, setDoc, addDoc,query,getDocs,onSnapshot } from "firebase/firestore";
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD8us3uTEnm7u43cqJHTVRCzaSHC2PzKNA",
-  authDomain: "markettoll-12722.firebaseapp.com",
-  databaseURL: "https://markettoll-12722-default-rtdb.firebaseio.com",
-  projectId: "markettoll-12722",
-  storageBucket: "markettoll-12722.firebasestorage.app",
-  messagingSenderId: "415697624629",
-  appId: "1:415697624629:web:bdb82c4ee69379c463db7c",
-  measurementId: "G-9BPJW8MKXF"
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  addDoc,
+  query,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
+import dotenv from "dotenv";
+dotenv.config();
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
 const db = getFirestore(app);
 
-export { app, db, collection, doc, setDoc, addDoc,query,getDocs,onSnapshot};
+export { app, db, collection, doc, setDoc, addDoc, query, getDocs, onSnapshot };
